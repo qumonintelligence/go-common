7f8086821bd0bd77a826721ba71d4cf2b23e666b
Kenny Chong <kenny.chong@qumonintelligence.com>
2020-02-19 12:16:48 +0800

    add lang.StringOf unknown

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/qumonintelligence/go-common/v2
 
 go 1.13
-
-require go.mongodb.org/mongo-driver v1.3.0
--- lang/string.go
+++ lang/string.go
@@ -1,6 +1,27 @@
 package lang
 
+import (
+	"fmt"
+)
+
 // IsEmptyString true if given string is nil or empty
 func IsEmptyString(s *string) bool {
 	return s == nil || len(*s) == 0
+}
+
+// StringOf try to get a string of unknown type
+func StringOf(unknown interface{}) string {
+
+	if unknown == nil {
+		return ""
+	}
+
+	switch value := unknown.(type) {
+	case string:
+		return value
+
+	default:
+		s := fmt.Sprintf("%v", unknown)
+		return s
+	}
 }
